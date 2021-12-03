32a5596ad9b8f64ddb496ebb2b621b81f35e54d5
Andreas Vida <andreas.vida@elitecoder.at>
2021-12-03 22:27:57 +0100

    added balance unit tests

--- src/test/java/clocking/TimeClockTest.java
+++ src/test/java/clocking/TimeClockTest.java
@@ -59,5 +59,83 @@ public class TimeClockTest {
 		Duration d = clock.getWorkedTime();		
 		assertEquals(Duration.ofMinutes(20), d);				
 	}
+	
+	@Test
+	public void simpleBalance() {
+		TimeClock clock = new TimeClockImpl();			
+		clock.clockIn(LocalDateTime.of(2000, 1, 1, 10, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 1, 10, 10).toInstant(ZoneOffset.ofHours(0)));		
+		clock.clockIn(LocalDateTime.of(2000, 1, 1, 11, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 1, 11, 10).toInstant(ZoneOffset.ofHours(0)));
+		
+		clock.setHours(new Duration[] {Duration.ofMinutes(10),
+				Duration.ofMinutes(10),
+				Duration.ofMinutes(10),
+				Duration.ofMinutes(10),
+				Duration.ofMinutes(10),
+				Duration.ofMinutes(10),
+				Duration.ofMinutes(10)});		
+		Duration balance = clock.getBalance(LocalDateTime.of(2000, 1, 1, 11, 10).toInstant(ZoneOffset.ofHours(0)));		
+		assertEquals(Duration.ofMinutes(20), balance);				
+	}
+	
+	@Test
+	public void balanceTwoDays() {
+		TimeClock clock = new TimeClockImpl();			
+		clock.clockIn(LocalDateTime.of(2000, 1, 1, 10, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 1, 10, 10).toInstant(ZoneOffset.ofHours(0)));		
+		clock.clockIn(LocalDateTime.of(2000, 1, 2, 11, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 2, 11, 10).toInstant(ZoneOffset.ofHours(0)));
+		
+		clock.setHours(new Duration[] {Duration.ofMinutes(0),
+				Duration.ofMinutes(0),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(0),
+				Duration.ofMinutes(0),
+				Duration.ofMinutes(0)});		
+		Duration balance = clock.getBalance(LocalDateTime.of(2000, 1, 2, 11, 10).toInstant(ZoneOffset.ofHours(0)));		
+		assertEquals(Duration.ofMinutes(-20), balance);				
+	}
+	
+	@Test
+	public void balanceThreeDays() {
+		TimeClock clock = new TimeClockImpl();
+		
+		// worked = 20 minutes.
+		clock.clockIn(LocalDateTime.of(2000, 1, 1, 10, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 1, 10, 10).toInstant(ZoneOffset.ofHours(0)));		
+		clock.clockIn(LocalDateTime.of(2000, 1, 2, 11, 0).toInstant(ZoneOffset.ofHours(0)));
+		clock.clockOut(LocalDateTime.of(2000, 1, 2, 11, 10).toInstant(ZoneOffset.ofHours(0)));
+		
+		clock.setHours(new Duration[] {Duration.ofMinutes(0),
+				Duration.ofMinutes(0),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(60),
+				Duration.ofMinutes(0),
+				Duration.ofMinutes(0)});		
+		Duration balance = clock.getBalance(LocalDateTime.of(2000, 1, 3, 11, 10).toInstant(ZoneOffset.ofHours(0)));		
+		assertEquals(Duration.ofMinutes(-80), balance);				
+	}
+	
+	@Test
+	public void balanceWithoutClockIn() {
+		TimeClock clock = new TimeClockImpl();
+		
+		// worked = 0 minutes.
+		
+		clock.setHours(new Duration[] {Duration.ofMinutes(0),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20),
+				Duration.ofMinutes(20)});		
+		Duration balance = clock.getBalance(LocalDateTime.of(2000, 1, 3, 11, 10).toInstant(ZoneOffset.ofHours(0)));		
+		assertEquals(Duration.ofMinutes(0), balance);				
+	}
+	
+	
 
 }
